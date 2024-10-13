68719dbbbbf64bffd129f2249465388ed510c346
agotori29919 <agotori29919@ucumberlands.edu>
2024-10-12 23:14:11 -0500

    Add files via upload

---
+++ Code Snippet of Prototype Implementation.py
@@ -0,0 +1,20 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            low = mid + 1
+
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            high = mid - 1
+
+        # x is present at mid
+        else:
+            return mid
+    return -1  # x is not present in the array
